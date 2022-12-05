54d3111d3bd2460187393660819714014c2705d7
Aggio98 <nigelrizzo@gmail.com>
2022-12-05 13:45:08 +0100

    redux store added and dependencies added

---
+++ src/store/index.js
@@ -0,0 +1,15 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import memeReducer from "./memes/slice"
+import userReducer from "./user/slice";
+import appStateReducer from "./appState/slice";
+
+const store = configureStore({
+  reducer: {
+    memes: memeReducer
+    user: userReducer,
+    appState: appStateReducer,
+  },
+});
+
+export default store;
